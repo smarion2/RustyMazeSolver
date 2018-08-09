7709b37780b1318131c5dedddf2f82e8959d3cd8
smarion2 <smarion2@gmail.com>
2018-08-08 20:47:02 -0400

    checking each direction need to start up the threads

--- src/infurstructure/maze_solver.rs
+++ src/infurstructure/maze_solver.rs
@@ -13,17 +13,53 @@ pub fn search_path(mut maze: &mut ProcessedMaze, node_id: u32) {
     }
 
     let node = Node::clone(&maze.maze_nodes[node_id as usize]);
+    let mut new_node_id = 0;
 
-    if node.left_wall == true && node.num_of_openings < 3 {        
+    if node.left_wall == true {        
         {
             let move_left = &mut maze.maze_nodes[transverse(Direction::Left, node_id, maze.nodes_per_row)];
             move_left.from_node_id = node_id;
             move_left.right_wall = false;
-            move_left.num_of_openings = move_left.num_of_openings - 1;
+            move_left.num_of_openings -= 1;
+            new_node_id = move_left.node_id;
         }
-        
-        search_path(&mut maze, 0);
     }
+    else if node.right_wall == true {
+        {
+            let move_right = &mut maze.maze_nodes[transverse(Direction::Right, node_id, maze.nodes_per_row)];
+            move_right.from_node_id = node_id;
+            move_right.left_wall = false;
+            move_right.num_of_openings -= 1;
+            new_node_id = move_right.node_id;
+        }
+    }
+    else if node.top_wall == true {
+        {
+            let move_up = &mut maze.maze_nodes[transverse(Direction::Up, node_id, maze.nodes_per_row)];
+            move_up.from_node_id = node_id;
+            move_up.bot_wall = false;
+            move_up.num_of_openings -= 1;
+            new_node_id = move_up.node_id;
+        }
+    }
+    else if node.bot_wall == true {
+        {
+            let move_down = &mut maze.maze_nodes[transverse(Direction::Down, node_id, maze.nodes_per_row)];
+            move_down.from_node_id = node_id;
+            move_down.top_wall = false;
+            move_down.num_of_openings -= 1;
+            new_node_id = move_down.node_id;
+        }
+    }
+
+    if node.num_of_openings < 3 {
+        search_path(&mut maze, new_node_id);
+    }
+    else {
+        // make thread and go.
+    }
+
+
 }
 
 pub fn transverse(direction: Direction, node_id: u32, nodes_per_row: u32) -> usize {
